474f6d44f1b1cbcfb97f8729e57f2791d1b25703
Tyler Bumpus <tbumpus13@gmail.com>
2022-06-29 20:55:23 -0400

    Add project files.

---
+++ FinalProject/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FinalProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
