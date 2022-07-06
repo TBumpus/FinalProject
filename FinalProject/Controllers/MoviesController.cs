bb3907f1cf10b3faf8e4709886598530cf1a43a2
Tyler Bumpus <tbumpus13@gmail.com>
2022-07-06 19:23:56 -0400

    added controller

---
+++ FinalProject/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinalProject.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: MoviesController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
